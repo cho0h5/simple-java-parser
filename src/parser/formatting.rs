10e4900f6ed47ccf91bc715de5d2aa0e435f9663
Youngho Cho <dominoyh5@gmail.com>
2023-05-29 16:44:29 +0900

    eleminate warning

--- src/parser/formatting.rs
+++ src/parser/formatting.rs
@@ -25,35 +25,34 @@ pub struct Tree(pub Node);
 
 impl fmt::Display for Tree {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.fmt(f, 0, true, vec![]);
+        self.0.fmt(f, true, vec![]);
         Ok(())
     }
 }
 
 impl Node {
-    fn fmt(&self, f: &mut fmt::Formatter, n: usize, isLast: bool, bridge: Vec<bool>) {
+    fn fmt(&self, f: &mut fmt::Formatter, is_last: bool, bridge: Vec<bool>) {
         match self {
             Terminal(token) => {
                 for b in &bridge[..bridge.len()-1] {
-                    write!(f, "{}", if *b { "│   " } else { "    " });
+                    write!(f, "{}", if *b { "│   " } else { "    " }).ok();
                 }
-                write!(f, "{}", if isLast { "└── " } else { "├── " });
-                write!(f, "{:?}\n", token)
+                write!(f, "{}", if is_last { "└── " } else { "├── " }).ok();
+                write!(f, "{:?}\n", token).ok();
             },
             NonTerminal(token, children) => {
-                if n != 0 {
+                if bridge.len() != 0 {
                     for b in &bridge[..bridge.len()-1] {
-                        write!(f, "{}", if *b { "│   " } else { "    " });
+                        write!(f, "{}", if *b { "│   " } else { "    " }).ok();
                     }
-                    write!(f, "{}", if isLast { "└── " } else { "├── " });
+                    write!(f, "{}", if is_last { "└── " } else { "├── " }).ok();
                 }
-                write!(f, "\x1b[36m{:?}\x1b[97m\n", token);
+                write!(f, "\x1b[36m{:?}\x1b[97m\n", token).ok();
                 for i in 0..children.len() {
                     let mut _bridge = bridge.clone();
                     _bridge.push(i != children.len() - 1);
-                    children[i].fmt(f, n + 1, i == children.len() - 1, _bridge);
+                    children[i].fmt(f, i == children.len() - 1, _bridge);
                 }
-                Ok(())
             }
         };
     }
