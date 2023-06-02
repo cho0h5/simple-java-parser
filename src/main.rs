fe07e92858ea76fc812f14f96efb8c646b93e1e7
Youngho Cho <dominoyh5@gmail.com>
2023-06-02 19:28:10 +0900

    main.rs 정리

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,53 @@ use parser::ParsingError;
 
 fn main() {
     let filename = match env::args().nth(1) {
-        Some(filename) => filename,
+        Some(filename) => {
+            print!("\x1b[32m[1/4]\x1b[37m ");
+            println!("File name: {}\n", filename);
+            filename
+        },
         None => {
-            println!("\x1b[31merror\x1b[37m: no input file");
+            println!("\x1b[31m[1/4] error\x1b[37m: no input file");
             process::exit(1);
         },
     };
-    println!("File name: {}\n", filename);
 
     let raw_contents = match fs::read_to_string(filename) {
-        Ok(contents) => contents,
+        Ok(contents) => {
+            print!("\x1b[32m[2/4]\x1b[37m ");
+            println!("File contents: \n{}\n", contents.trim());
+            contents
+        },
         Err(_) => {
-            println!("\x1b[31merror\x1b[37m: something went wrong during reading file");
+            println!("\x1b[31m[2/4] error\x1b[37m: something went wrong during reading file");
             process::exit(1);
         },
     };
 
     let tokens = match token_reader::read_tokens(&raw_contents) {
-        Ok(tokens) => tokens,
+        Ok(tokens) => {
+            print!("\x1b[32m[3/4]\x1b[37m ");
+            println!("Read tokens:\n{}\n", tokens);
+            tokens
+        },
         Err(UnknownTokenError(token)) => {
-            println!("\x1b[31merror\x1b[37m: unknown token: {}", token);
+            println!("\x1b[31m[3/4] error\x1b[37m: unknown token: {}", token);
             process::exit(1);
         },
     };
-    println!("Read tokens:\n{}\n", tokens);
 
-    let parse_tree = match parser::parse(tokens) {
-        Ok(tree) => tree,
+    match parser::parse(tokens) {
+        Ok(tree) => {
+            print!("\x1b[32m[4/4]\x1b[37m ");
+            println!("Parse tree:\n{}", tree);
+            println!("Accepted!");
+            tree
+        },
         Err(ParsingError(expected, found)) => {
-            println!("\x1b[31merror\x1b[37m: parsing error");
+            println!("\x1b[31m[4/4] error\x1b[37m: parsing error");
             println!("\texpected: {:?}", expected);
             println!("\tbut found: {:?}", found);
             process::exit(1);
         },
     };
-    println!("Parse tree:\n{}", parse_tree);
 }
-
